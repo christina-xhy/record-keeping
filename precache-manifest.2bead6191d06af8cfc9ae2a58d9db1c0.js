e42f08a27e8cd2e6af28ff808a844c2634307958
christina <christinayxh@163.com>
2023-01-16 00:26:16 +0800

    deploy

---
+++ precache-manifest.2bead6191d06af8cfc9ae2a58d9db1c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e1c26b07454525c5dca3c5b8e95237",
+    "url": "/record-keeping/index.html"
+  },
+  {
+    "revision": "cb1c438d534c1dad18fe",
+    "url": "/record-keeping/static/css/main.c29a87bc.chunk.css"
+  },
+  {
+    "revision": "e434dd9637c5b1f16b3b",
+    "url": "/record-keeping/static/js/2.6105103c.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/record-keeping/static/js/2.6105103c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1c438d534c1dad18fe",
+    "url": "/record-keeping/static/js/main.77bb40e5.chunk.js"
+  },
+  {
+    "revision": "ad732c1b07b8124da122",
+    "url": "/record-keeping/static/js/runtime-main.f2f550a1.js"
+  }
+]);
